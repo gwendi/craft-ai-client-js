85ca0add114489757186ec76c3556b1781ef5855
Clodéric Mars <cloderic.mars@craft.ai>
2017-06-19 10:19:16 +0200

    Adding the tests for properties.js

--- test/browser/test.js
+++ test/browser/test.js
@@ -10,6 +10,7 @@ require('../test_errors');
 require('../test_getAgent');
 require('../test_getAgentDecisionTree');
 require('../test_getSharedAgentInspectorUrl');
+require('../test_properties');
 require('../test_time');
 
 if (window.initMochaPhantomJS) {
